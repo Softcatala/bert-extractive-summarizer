a3eb8165c790a869812749e3a5b4c9dd0fb2f391
Jordi Mas <jmas@softcatala.org>
2021-06-13 22:26:07 +0200

    Fixes

--- server.py
+++ server.py
@@ -81,47 +81,36 @@ def convert_raw_text_by_sent():
 
 
 def init():
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('-model', dest='model', default='bert-base-uncased', help='The model to use')
-    parser.add_argument('-transformer-type',
-                        dest='transformer_type', default=None,
-                        help='Huggingface transformer class key')
-    parser.add_argument('-transformer-key', dest='transformer_key', default=None,
-                        help='The transformer key for huggingface. For example bert-base-uncased for Bert Class')
-    parser.add_argument('-greediness', dest='greediness', help='', default=0.45)
-    parser.add_argument('-reduce', dest='reduce', help='', default='mean')
-    parser.add_argument('-hidden', dest='hidden', help='', default=-2)
-    ##parser.add_argument('-port', dest='port', help='', default=8080)
-    ##parser.add_argument('-host', dest='host', help='', default='0.0.0.0')
 
-    args = parser.parse_args()
-    
-#    nltk.download('punkt')
+    model = 'bert-base-uncased'
+    transformer_type = None
+    transformer_key = None
+    greediness = 0.45
+    reduce = 'mean'
+    hidden = -2
 
-
-    if args.transformer_type is not None:
-        print(f"Using Model: {args.transformer_type}")
-        assert args.transformer_key is not None, 'Transformer Key cannot be none with the transformer type'
+    if transformer_type is not None:
+        print(f"Using Model: {transformer_type}")
+        assert transformer_key is not None, 'Transformer Key cannot be none with the transformer type'
 
         summarizer = TransformerSummarizer(
-            transformer_type=args.transformer_type,
-            transformer_model_key=args.transformer_key,
-            hidden=int(args.hidden),
-            reduce_option=args.reduce
+            transformer_type=transformer_type,
+            transformer_model_key=transformer_key,
+            hidden=int(hidden),
+            reduce_option=reduce
         )
 
     else:
-        print(f"Using Model: {args.model}")
+        print(f"Using Model: {model}")
 
         summarizer = Summarizer(
-            model=args.model,
-            hidden=int(args.hidden),
-            reduce_option=args.reduce
+            model=model,
+            hidden=int(hidden),
+            reduce_option=reduce
         )
 
     #app.run(port=int(args.port))
     #app.run(host=args.host, port=int(args.port))
-
 
 
 if __name__ == '__main__':
